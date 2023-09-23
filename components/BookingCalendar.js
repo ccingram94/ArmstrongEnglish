484c2e93eed3ed14a96abda6b7df7f116906edfd
Constance!! <34844921+ccingram94@users.noreply.github.com>
2023-09-23 11:14:21 +0000

    updating css for react calendar

--- components/BookingCalendar.js
+++ components/BookingCalendar.js
@@ -9,8 +9,8 @@ function BookingCalendar() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div className='flex items-center justify-center drop-shadow-2xl py-8 px-4 rounded-xl bg-white h-full w-full lg:w-[50vw] rounded-xl flex flex-col justify-center items-center'>
-      <Calendar className="z-[-10]" onChange={onChange} value={value}
+    <div className='flex static items-center justify-center drop-shadow-2xl py-8 px-4 rounded-xl bg-white h-full w-full md:w-[50vw] rounded-xl flex flex-col justify-center items-center'>
+      <Calendar className="z-[-10" onChange={onChange} value={value}
         maxDate={add(new Date(), 30)}
        />
     </div>
