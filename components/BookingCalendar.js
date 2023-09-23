33c1399325783ef935ca5f31924aef752ad8262d
Constance!! <34844921+ccingram94@users.noreply.github.com>
2023-09-23 01:59:16 +0000

    added date-fns to handle scheduling and time zone localization

--- components/BookingCalendar.js
+++ components/BookingCalendar.js
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import Calendar from 'react-calendar';
+import { format, add, formatDistance, formatRelative, subDays, addDays } from 'date-fns';
 
 
 function BookingCalendar() {
@@ -10,7 +11,8 @@ function BookingCalendar() {
   return (
     <div className='flex items-center justify-center drop-shadow-2xl py-8 px-4 rounded-xl bg-white h-full w-full lg:w-[50vw] rounded-xl flex flex-col justify-center items-center'>
       <Calendar className="z-[-10]" onChange={onChange} value={value}
-      minDate={new Date(now())}
+        defaultView={month} maxDetail={month}
+        maxDate={add(new Date(), 30)}
        />
     </div>
   );
