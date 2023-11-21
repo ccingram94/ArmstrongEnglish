3599ec26e7fd38b886614e6da4301f01c12cad87
Constance!! <34844921+ccingram94@users.noreply.github.com>
2023-11-21 16:21:55 +0000

    css and spacing stuff

--- components/BookingCalendar.js
+++ components/BookingCalendar.js
@@ -18,26 +18,26 @@ function BookingCalendar() {
   const timeZone = value;
 
   return (
-    <>
+    <div className='w-full flex flex-row flex-wrap justify-center text-center items-start'>
       <div className='w-full lg:w-1/3 flex flex-row flex-wrap m-2 lg:p-2 drop-shadow-2xl rounded-xl bg-white justify-center items-center content-center'>
         <Calendar onChange={onChange} value={value} className="p-4 rounded-xl m-2"
           maxDate={addDays(new Date(), 60)} minDate={ addDays(new Date(), 1) }
         />
       </div>
-      <div className='w-1/2 md:w-1/4 flex flex-col m-2 p-2 drop-shadow-2xl rounded-xl bg-white justify-center items-center content-center'>
+      <div className='w-full md:w-1/4 flex flex-col m-2 p-2 drop-shadow-2xl rounded-xl bg-white justify-center items-center content-center'>
         <h3 className='text-xl lg:text-2xl font-extrabold text-violet-900 p-2 m-2'>Classes Today</h3>
         <button className='p-2 m-2 lg:text-xl font-extrabold text-white bg-violet-900 rounded-xl drop-shadow-xl opacity-80 hover:opacity-100 transition-all'>
           Option 1
         </button>
       </div>
-      <div className='w-1/2 md:w-1/4 flex flex-row flex-wrap m-2 p-2 drop-shadow-2xl rounded-xl bg-white justify-center items-center text-center'>
+      <div className='w-full md:w-1/4 flex flex-col m-2 p-2 drop-shadow-2xl rounded-xl bg-white justify-center items-center text-center'>
         <h3 className='text-xl lg:text-2xl font-extrabold text-violet-900 p-2 m-2'>Selected Classes</h3>
         <p className='lg:text-xl font-bold text-violet-900/90 p-2 m-2'>no classes selected</p>
         <button className='p-2 m-2 lg:text-xl font-extrabold text-white bg-gradient-to-br from-violet-800 to-indigo-800 rounded-xl opacity-80 hover:opacity-100 transition-all drop-shadow-xl'>
           Book Now
         </button>
       </div>
-    </>
+    </div>
   );
 }
 
