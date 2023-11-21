39baa030e952bbff66ee23709318d335b6d79697
Constance!! <34844921+ccingram94@users.noreply.github.com>
2023-11-21 17:56:19 +0000

    updating css

--- app/beginner/page.js
+++ app/beginner/page.js
@@ -9,51 +9,57 @@ import a1grammardata from "../../a1grammardata";
 function Beginner() {
   const { t, i18n } = useTranslation();
   return (
-    <div className="bg-violet-900 w-full text-center justify-center items-center">
-      <div className="w-full h-full bg-hero bg-cover bg-center">
-        <div className="bg-violet-900/90">
-        <h1 className="text-3xl p-2 pt-6 font-extrabold text-white">{t('Beginner Lessons')}</h1>
-        <h2 className="text-xl p-2 font-bold text-white/70">{t('CEFR Levels A1 and A2')}</h2>
+    <div className="bg-hero bg-cover bg-center w-full">
+      <div className="w-full min-h-screen bg-violet-900/90 text-center items-center">
+
+        <div className="w-full h-full">
+          <div className="">
+          <h1 className="text-4xl p-2 pt-12 font-extrabold text-white">{t('Beginner Lessons')}</h1>
+          <h2 className="text-xl p-2 font-bold text-white/70">{t('CEFR Levels A1 and A2')}</h2>
+          </div>
+        </div>
+
+        <div className="h-full flex flex-row flex-wrap justify-center p-2">
+          <div className="flex flex-col h-full lg:w-1/2 text-center items-center justify-center">
+            <h3 className="text-xl p-4 m-2 font-bold text-white">{t('Level A1 Lessons')} </h3>
+            <p className="text-sm max-w-md p-2 m-2 font-bold text-white/90">{t('CEFR A1 is the first step in learning English.  Students can understand and use basic words and sentences for everyday conversations.')} </p>
+            <div className="flex flex-wrap justify-center container mx-auto w-fit">
+            { Object.keys(a1data).map((key, index) => (
+                  <Link href={"/lesson/" + index} className="w-full md:w-fit bg-white/90 hover:bg-white transition-all rounded-md shadow-xl p-6 m-2" key={index}>
+                    <p className="text-sm font-extrabold text-violet-900/80">
+                    {t('Lesson')} { a1data[key].id }
+                    </p>
+                    <p className="text-xl font-extrabold text-violet-900">
+                    { a1data[key].title }
+                    </p>
+                    <p className="text-sm font-bold text-violet-900/80">
+                      {t(`${a1data[key].title}`)}
+                    </p>
+                  </Link>
+              ))}
+            </div>
+            <div className="flex flex-wrap justify-center container mx-auto w-fit m-2">
+            { Object.keys(a1grammardata).map((key, index) => (
+                  <div className="bg-white/90 rounded-xl shadow-xl p-2 m-2" key={index}>
+                    <p className="text-lg font-bold p-2 text-indigo-700">
+                    { a1grammardata[key].title }
+                    </p>
+                  </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col h-full lg:w-1/2 text-center items-center">
+            <h3 className="text-xl p-4 m-2 font-bold text-white">{t('Level A2 Lessons')}</h3>
+            <p className="text-sm max-w-md p-2 m-2 font-bold text-white/90">{t('CEFR A2 is the next level of English language learning, where learners can handle simple, everyday tasks and have basic conversations about familiar topics.')} </p>
+            <h3 className="text-xl font-bold text-indigo-100/70">{t('Coming Soon')}</h3>
+            <div className="flex flex-wrap pb-12 justify-center container mx-auto w-fit">
+              <LockClosedIcon className="h-32 m-4 text-indigo-100/70" />
+            </div>
+          </div>
         </div>
       </div>
-      <div className="h-full flex flex-row flex-wrap justify-center p-2 m-2">
-        <div className="h-full lg:w-1/2">
-          <h3 className="text-xl p-4 m-2 font-bold text-white">{t('Level A1 Lessons')} </h3>
-          <p className="text-sm p-2 m-2 font-bold text-white/90">{t('CEFR A1 is the first step in learning English.  Students can understand and use basic words and sentences for everyday conversations.')} </p>
-          <div className="flex flex-wrap justify-center container mx-auto w-fit">
-          { Object.keys(a1data).map((key, index) => (
-                <Link href={"/lesson/" + index} className="bg-white/90 rounded-xl shadow-xl p-6 m-2" key={index}>
-                  <p className="text-sm font-extrabold text-indigo-700/80">
-                  {t('Lesson')} { a1data[key].id }
-                  </p>
-                  <p className="text-xl font-extrabold text-indigo-700">
-                  { a1data[key].title }
-                  </p>
-                  <p className="text-sm font-bold text-indigo-700/50">
-                    {t(`${a1data[key].title}`)}
-                  </p>
-                </Link>
-            ))}
-          </div>
-          <div className="flex flex-wrap justify-center container mx-auto w-fit m-2">
-          { Object.keys(a1grammardata).map((key, index) => (
-                <div className="bg-white/90 rounded-xl shadow-xl p-2 m-2" key={index}>
-                  <p className="text-lg font-bold p-2 text-indigo-700">
-                  { a1grammardata[key].title }
-                  </p>
-                </div>
-            ))}
-          </div>
-        </div>
-        <div className="h-full lg:w-1/2">
-          <h3 className="text-xl p-4 m-2 font-bold text-white">{t('Level A2 Lessons')}</h3>
-          <p className="text-sm p-2 m-2 font-bold text-white/90">{t('CEFR A2 is the next level of English language learning, where learners can handle simple, everyday tasks and have basic conversations about familiar topics.')} </p>
-          <h3 className="text-xl font-bold text-indigo-100/70">{t('Coming Soon')}</h3>
-          <div className="flex flex-wrap pb-12 justify-center container mx-auto w-fit">
-            <LockClosedIcon className="h-32 m-4 text-indigo-100/70" />
-          </div>
-        </div>
-      </div>
+      
+      
       <div>
     </div>
   </div>
--- app/class/page.js
+++ app/class/page.js
@@ -3,17 +3,19 @@ import { CheckCircleIcon } from "@heroicons/react/20/solid";
 
 function Class() {
   return (
-    <div className="bg-violet-900 min-h-screen w-full">
-      <div className="w-full h-full bg-hero bg-cover bg-center">
-        <div className="flex flex-col justify-center text-center bg-violet-900/90">
-          <h1 className="text-3xl p-2 pt-6 font-extrabold text-white">Book a Class</h1>
-          <h2 className="text-xl p-2 font-bold text-white/70">One Class (25 Minutes) $25</h2>
+    <div className="bg-hero bg-cover bg-center w-full">
+      <div className="w-full h-full bg-violet-900/90 min-h-screen">
+        <div className="w-full h-full">
+          <div className="flex flex-col justify-center text-center">
+            <h1 className="text-3xl p-2 pt-6 font-extrabold text-white">Book a Class</h1>
+            <h2 className="text-xl p-2 font-bold text-white/70">One Class (25 Minutes) $25</h2>
+          </div>
         </div>
-      </div>
-      {/* Top Section: Booking Calendar */}
-      <div className="w-full">
-        <div className="flex flex-row flex-wrap justify-center items-center">
-          <BookingCalendar />
+        {/* Top Section: Booking Calendar */}
+        <div className="w-full h-full">
+          <div className="flex flex-row flex-wrap justify-center items-center">
+            <BookingCalendar />
+          </div>
         </div>
       </div>
     </div>
