b308614afb44c752ca8e55e7e4ea4792e006972d
Constance!! <34844921+ccingram94@users.noreply.github.com>
2023-11-22 14:45:35 +0000

    adding calendly scheduling stuff

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -23,17 +23,6 @@ const DynamoDBClientConfig = {
   export default NextAuth({
     // Configure one or more authentication providers
     providers: [
-      EmailProvider({
-        server: {
-          host: process.env.EMAIL_SERVER_HOST,
-          port: process.env.EMAIL_SERVER_PORT,
-          auth: {
-            user: process.env.EMAIL_SERVER_USER,
-            pass: process.env.EMAIL_SERVER_PASSWORD
-          }
-        },
-        from: process.env.EMAIL_FROM
-      }),
       // ...add more providers here
     ],
     adapter: DynamoDBAdapter(
--- app/class/page.js
+++ app/class/page.js
@@ -4,10 +4,12 @@ import { InlineWidget } from "react-calendly";
 
 function Class() {
   const [ loading, setLoading ] = useState(true);
+  const [ scheduled, setScheduled ] = useState(false);
+  const [ classInfo, setClassInfo ] = useState('');
    
   function isCalendlyEvent(e) {
     return e.data.event;
-  };
+  }
    
   window.addEventListener(
     'message',
@@ -17,6 +19,10 @@ function Class() {
       }
       if (e.data.event == 'calendly.event_type_viewed') {
         setLoading(false);
+      }
+      if (e.data.event == 'calendly.event_scheduled') {
+        setClassInfo(e.data.payload.event.uri)
+        setScheduled(true);
       }
     }
   );
@@ -30,6 +36,27 @@ function Class() {
             <h2 className="text-xl p-2 font-bold text-white/70">One Class (25 Minutes) $25</h2>
           </div>
           { loading && <p className='bg-white w-fit rounded-md drop-shadow-xl p-6 text-violet-900 text-2xl font-extrabold text-center'>Loading Calendar...</p>}
+          { scheduled && 
+            <div className="bg-white text-center rounded-md drop-shadow-xl p-6 text-violet-900">
+              <p className='text-2xl font-extrabold'>Class Scheduled</p>
+              <p className='text-xl font-bold'>Please save your class information.</p>
+              <p>{ classInfo }</p>
+              <div className='flex flex-row flex-wrap justify-center items-center text-center p-2 m-2'>
+                <div className='p-2'>
+                  <p className='text-xl font-extrabold'>Payment</p>
+                  <p>Click the button below or scan the QR code to be taken to the payment page.
+                    Unpaid classes will be cancelled.
+                  </p>
+                </div>
+                <div className='p-2'>
+                  <p className='text-xl font-extrabold'>Join ClassIn</p>
+                  <p>Click the button below or scan the QR code to join Mrs. Armstrong on ClassIn.
+                    All classes are delivered online via ClassIn.
+                  </p>
+                </div>
+              </div>
+            </div>
+          }
         </div>
         <InlineWidget url="https://calendly.com/constanceingram94/25min" />
       </div>
