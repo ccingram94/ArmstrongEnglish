95c66a5be81d4c33ff0e9b43dda65f1f41d18b85
Constance!! <34844921+ccingram94@users.noreply.github.com>
2023-12-09 14:12:06 +0000

    adding lesson data

--- app/lesson/[slug]/page.js
+++ app/lesson/[slug]/page.js
@@ -13,21 +13,25 @@ function Lesson({ params } ) {
   const { t, i18n } = useTranslation();
   const lesson = a1data[params.slug];
   const vocablist = lesson.vocab;
+  const [ currentWord, setCurrentWord ] = useState(vocablist[0])
+  const [ currentTranslation, setCurrentTranslation ] = useState(vocablist[0])
   return (
     <>
     <div className="flex flex-col lg:flex-row bg-violet-900 w-full text-center">
       <div className="w-full lg:w-1/3 flex flex-col justify-center items-center text-center bg-violet-50 text-violet-900 p-2">
-        <p className="font-bold">Lesson {lesson.id}</p>
+        <p className="font-bold pt-12">Lesson {lesson.id}</p>
         <h2 className="text-3xl font-extrabold p-2 m-2">{lesson.title}</h2>
+        <div className='text-sm p-2 m-2'>
+          <p className='p-2'>{lesson.lessontext}</p>
+        </div>
       </div>
       <div className="w-full lg:w-2/3 bg-hero bg-cover bg-center">
         <div className="flex flex-col justify-center items-center w-full h-max bg-violet-600/80">
           <div className="flex flex-row flex-wrap justify-center items-center text-center w-2/3 h-full md:h-96 bg-white text-violet-900 rounded-sm shadow-xl p-4 m-6">
-            <Image src={'https://d20etpig7obrzj.cloudfront.net/tefl160.jpg'} width={300} height={300} className='rounded-md m-1 shadow-xl'  />
             <div className="flex flex-col p-2 m-2 justify-center items-center text-center">
-              <p className="font-extrabold text-3xl p-2 m-2">Vocabulary</p>
-              <p className="font-bold text-2xl opacity-90">translation</p>
-              <PlayCircleIcon className="h-16 m-2" />
+              <p className="font-extrabold text-6xl m-2">{currentWord}</p>
+              <p className="font-bold text-2xl opacity-90 m-2">{currentTranslation}</p>
+              <PlayCircleIcon className="h-16 m-2 opacity-90 hover:opacity-100 transition-all drop-shadow-xl m-6" />
             </div>
           </div>
         </div>
@@ -45,6 +49,9 @@ function Lesson({ params } ) {
                   </p>
                 </div>
             ))}
+    {
+      
+    }
     </div>
   </div>
   </>
